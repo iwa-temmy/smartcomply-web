7e9beaf5b43140b4d7234536c7f1d020f1fa8f3e
olawalecodes <iwaloyeo@gmail.com>
2022-07-12 23:42:56 +0100

    [FEATURE]: Home page footer and audit process section completed

---
+++ components/nav/Nav.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import Button from "../button/Button";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+export default function Nav({ styles }) {
+  return (
+    <header
+      className={`${styles.nav} flex flex-col lg:px-20 lg:py-6 lg:flex-row lg:justify-between`}
+    >
+      <div className="p-3 flex justify-between">
+        <Image src="/logo.svg" alt="logo" width={120} height={50} />
+        <div className="flex lg:hidden">
+          <Button name={<GiHamburgerMenu color="#0C2356" size="24px"/>} styles={styles} />
+        </div>
+      </div>
+      <nav className="flex flex-col bg-slate-500 items-center lg:flex-row lg:items-center lg:bg-inherit">
+        <span className="my-4 lg:mr-10 text-white uppercase">
+          <Link href="https://app.smartcomplyapp.com/auth/login">Sign in</Link>
+        </span>
+        <span className="my-4 lg:mr-10 text-white uppercase">
+          <Link href="https://app.smartcomplyapp.com/auth/login">Blog</Link>
+        </span>
+        <Button name="Book Demo" styles={styles} />
+      </nav>
+    </header>
+  );
+}
---
+++ data.js
@@ -0,0 +1,26 @@
+export const auditsProcesses = [
+  {
+    id: 1,
+    image: "/pcidss.svg",
+    description:
+      "Ensure that your company is always in compliance with the Payment Card Industry Security Standards Council (PCI SSC) and give your customers full peace of mind as they carry out transactions on your platform.",
+  },
+  {
+    id: 2,
+    image: "/soc2.svg",
+    description:
+      "SOC 2 TYPE I & II: Manage your customer’s data based on the five “trust service principles”—security, availability, processing integrity, confidentiality and privacy. Easily generate up-to-date SOC 2 reports that establish trust in your brand.",
+  },
+  {
+    id: 3,
+    image: "/gdpr.svg",
+    description:
+      "Our GDPR compliance functionality helps you prove that you are collecting and processing personal information from individuals who leave in the European Union (EU) ethically.",
+  },
+  {
+    id: 4,
+    image: "/iso27001.svg",
+    description:
+      "Easily manage information risks, such as cyber-attacks, hacks, data leaks or theft by staying compliant at all times with the ISO 27001 international compliance standard.",
+  },
+];
