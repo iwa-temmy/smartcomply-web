a568ee2dbeb74d7d3bcf7c385e9d836fe18b8347
olawalecodes <iwaloyeo@gmail.com>
2022-07-12 07:45:49 +0100

    [configuration]:configured tailwind css

---
+++ components/mainFooter/MainFooter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MainFooter = () => {
+  return (
+    <footer className="flex justify-between">
+      <code>12</code>
+      <span>Welcome</span>
+    </footer>
+  );
+};
+
+export default MainFooter;
