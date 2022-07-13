4cc118010685c386d657753ef8e9175863b7e105
KodingNYoung <abiodunadebambo44@gmail.com>
2022-07-13 18:25:58 +0100

    [design]: design the view for the blog

--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
 export const truncateText = (str, noOfChar) => {
   return str.slice(0, noOfChar) + "...";
 };
+
+export const capitalizeText = str => {
+  return str[0].toUpperCase() + str.slice(1);
+};
