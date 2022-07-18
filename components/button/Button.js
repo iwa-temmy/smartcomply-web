5c7de92ce3796863daec6fd7cb5949cce96a9c3b
KodingNYoung <abiodunadebambo44@gmail.com>
2022-07-18 18:23:02 +0100

    [features]: revamping the website

--- components/button/Button.js
+++ components/button/Button.js
@@ -1,7 +1,12 @@
 const Button = ({ buttonAction, name, styles }) => {
   return (
     <>
-      <button className={styles + " py-3 px-5 bg-white rounded-lg uppercase my-5 lg:my-0" } onClick={buttonAction}>
+      <button
+        className={
+          styles + " py-3 px-5 bg-white rounded-lg uppercase my-5 lg:my-0"
+        }
+        onClick={buttonAction}
+      >
         {name}
       </button>
     </>
--- components/home/nav/Nav.js
+++ components/home/nav/Nav.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "../../../styles/Home.module.css";
+import styles from "styles/Home.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "../../button/Button";
@@ -12,34 +12,39 @@ export default function Nav() {
     setNavToggle(!navToggle);
   };
   return (
-    <header
-      className={`${styles.nav} flex flex-col relative lg:px-20 lg:py-4 lg:flex-row lg:justify-between`}
-    >
-      <div className="py-2 px-5 flex justify-between lg:p-3">
-        <Image src="/logo-white.svg" alt="logo" width={120} height={50} />
-        <div className="flex lg:hidden">
-          <Button
-            name={<GiHamburgerMenu color="#0C2356" size="24px" />}
-            styles={styles}
-            buttonAction={openNavMenu}
-          />
+    <header className={`${styles.nav} absolute w-full bg-transparent z-10`}>
+      <div className="container flex flex-col sm:items-center sm:flex-row sm:justify-between sm:pt-4">
+        <div className="py-2 px-5 flex justify-between items-center sm:p-3">
+          <Image src="/logo-white.svg" alt="logo" width={120} height={50} />
+          <div className="flex sm:hidden">
+            <button
+              className=" py-3 px-5 rounded-sm uppercase my-5 sm:my-0"
+              onClick={openNavMenu}
+            >
+              <GiHamburgerMenu color="#fff" size="24px" />
+            </button>
+          </div>
         </div>
+        <nav
+          className={`${
+            navToggle
+              ? "pt-3 w-full translate-x-0"
+              : "-translate-x-full flex-col bg-slate-500"
+          } flex flex-col bg-slate-500 transition-all duration-100 absolute top-24 h-80 sm:flex sm:flex-row items-center sm:bg-inherit sm:relative sm:top-0 sm:h-fit sm:translate-x-0 sm:items-center`}
+        >
+          <span className="my-8 sm:mr-10 text-white uppercase sm:my-4">
+            <Link href="https://app.smartcomplyapp.com/auth/login">
+              <a>Sign in</a>
+            </Link>
+          </span>
+          <span className="my-8 sm:mr-10 text-white uppercase sm:my-4">
+            <Link href="https://app.smartcomplyapp.com/auth/login">
+              <a>Blog</a>
+            </Link>
+          </span>
+          <Button name="Book Demo" styles={styles?.button} />
+        </nav>
       </div>
-      <nav
-        className={`${
-          navToggle
-            ? "pt-3 w-full translate-x-0"
-            : "-translate-x-full flex-col bg-slate-500"
-        } flex flex-col bg-slate-500 transition-all duration-100 absolute top-24 h-80 lg:flex lg:flex-row items-center lg:bg-inherit lg:relative lg:top-0 lg:h-fit lg:translate-x-0 lg:items-end`}
-      >
-        <span className="my-8 lg:mr-10 text-white uppercase lg:my-4">
-          <Link href="https://app.smartcomplyapp.com/auth/login">Sign in</Link>
-        </span>
-        <span className="my-8 lg:mr-10 text-white uppercase lg:my-4">
-          <Link href="https://app.smartcomplyapp.com/auth/login">Blog</Link>
-        </span>
-        <Button name="Book Demo" styles={styles?.button} />
-      </nav>
     </header>
   );
 }
